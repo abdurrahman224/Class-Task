68073014907c592c88fc46a7eb30275e12fa54cb
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-10-16 09:45:04 +0600

    Class Method

--- BasicClass.js
+++ BasicClass.js
@@ -3,8 +3,29 @@ constructor(brand){
 
 this.brand = brand
 
+
+
+
+}
+honk(){
+
+
+    console.log("Beep beep! ");
+    
 }
 }
+
+
+
+let myCar = new Car ('Mercedes-Benz')
+
+console.log(myCar.brand);
+
+
+myCar.honk()
+
+
+
 
 const brandInput = document.getElementById('brandInput');
 const saveBtn = document.getElementById('saveBtn')
